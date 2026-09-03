f93f259e0ae8d8139ff3e42a4029b5d2c958d3a6
axbrogi23 <ax.brogi@duocuc.cl>
2026-09-02 22:45:06 -0400

    creacion de login html y el un login de js mas estilos en css

---
+++ login.js
@@ -0,0 +1,94 @@
+const formularioLogin = document.getElementById("login-form");
+
+
+formularioLogin.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+
+    const correo = document.getElementById("correo").value.trim();
+
+    const password = document.getElementById("password").value;
+
+
+    const errorCorreo = document.getElementById("error-correo");
+
+    const errorPassword = document.getElementById("error-password");
+
+
+    // Limpiar mensajes anteriores
+
+    errorCorreo.textContent = "";
+
+    errorPassword.textContent = "";
+
+
+    let formularioValido = true;
+
+
+    // DOMINIOS PERMITIDOS
+
+    const dominiosPermitidos = ["@duoc.cl", "@profesor.duoc.cl","@gmail.com"];
+    
+
+    const correoValido = dominiosPermitidos.some(function(dominio) {
+
+        return correo.endsWith(dominio);
+
+    });
+
+
+    // VALIDAR CORREO
+
+    if (correo === "") {
+
+        errorCorreo.textContent =
+            "El correo es obligatorio.";
+
+        formularioValido = false;
+
+    } else if (correo.length > 100) {
+
+        errorCorreo.textContent =
+            "El correo no puede superar los 100 caracteres.";
+
+        formularioValido = false;
+
+    } else if (!correoValido) {
+
+        errorCorreo.textContent =
+            "Solo se permiten correos @duoc.cl, @profesor.duoc.cl o @gmail.com.";
+
+        formularioValido = false;
+
+    }
+
+
+    // VALIDAR CONTRASEÑA
+
+    if (password === "") {
+
+        errorPassword.textContent =
+            "La contraseña es obligatoria.";
+
+        formularioValido = false;
+
+    } else if (password.length < 4 || password.length > 10) {
+
+        errorPassword.textContent =
+            "La contraseña debe tener entre 4 y 10 caracteres.";
+
+        formularioValido = false;
+
+    }
+
+
+    // LOGIN CORRECTO
+
+    if (formularioValido) {
+
+        alert("Inicio de sesión correcto");
+
+    }
+
+});
